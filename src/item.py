9cccdf410990cef3419658b59caeaf2972a41137
vvvadim <v.vladimirov@it-park.tech>
2023-09-21 15:51:17 +0300

    first hw

--- src/item.py
+++ src/item.py
@@ -6,25 +6,16 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+    def __repr__(self):
+        return f'Item(name={self.name}, price={self.price}, quantity={self.quantity})'
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
-        """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+        self.price *= self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+test_item = Item('Test', 9000, 3)
+test_item.pay_rate = 0.9
+
+
+def test_calculate_total_price():
+    assert test_item.price * test_item.quantity == test_item.calculate_total_price()
+
+
+def test_apply_discount():
+    native_price = test_item.price * test_item.pay_rate
+    test_item.apply_discount()
+    price = test_item.price
+    assert native_price == price
