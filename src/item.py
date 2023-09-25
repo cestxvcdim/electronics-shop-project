bfa93290ab71c423110d318d53073dbe61541ca6
vvvadim <v.vladimirov@it-park.tech>
2023-09-25 17:37:58 +0300

    second hw

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -6,16 +9,39 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        self.name = name
+        if len(name) > 10:
+            name = name[:10]
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Item(name={self.name}, price={self.price}, quantity={self.quantity})'
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        if len(new_name) > 10:
+            new_name = new_name[:10]
+        self.__name = new_name
 
     def calculate_total_price(self) -> float:
         return self.price * self.quantity
 
     def apply_discount(self) -> None:
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        with open(path, 'r', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'], row['price'], row['quantity'])
+
+    @staticmethod
+    def string_to_number(str_num: str) -> int:
+        return int(float(str_num))
--- tests/test_item.py
+++ tests/test_item.py
@@ -15,3 +15,14 @@ def test_apply_discount():
     test_item.apply_discount()
     price = test_item.price
     assert native_price == price
+
+
+def test_name():
+    test_item.name = 'new_name'
+    assert test_item.name == 'new_name'
+    test_item.name = '123456789qqqqqqqq'
+    assert test_item.name == '123456789q'
+
+
+def test_string_to_number():
+    assert 89 == test_item.string_to_number('89.9992')
